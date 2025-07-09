80fddbb9b8036484bad7552a2d0e33ee63c703e0
Pablo Zurita <pablozurita.jr@outlook.com>
2025-07-09 09:26:45 -0500

    Creación del componente calculadora y configuración

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CalculadoraImc} from './calculadora-imc/calculadora-imc';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'calculadora', redirectTo: 'calculadora-imc', pathMatch: 'full' },
+];
---
+++ src/app/calculadora-imc/calculadora-imc.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora-imc',
+  imports: [],
+  templateUrl: './calculadora-imc.html',
+  styleUrl: './calculadora-imc.css'
+})
+export class CalculadoraImc {
+
+}
