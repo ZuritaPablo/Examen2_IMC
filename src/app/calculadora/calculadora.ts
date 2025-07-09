a2dc34074dd20efc4c57e760294e8f7622b09ce3
Pablo Zurita <pablozurita.jr@outlook.com>
2025-07-09 10:23:53 -0500

    ULtimos retoques

--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -1,8 +1,9 @@
-import { DecimalPipe } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
+  standalone: true,
   imports: [DecimalPipe],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.css'
@@ -10,17 +11,19 @@ import { Component, computed, signal } from '@angular/core';
 export class Calculadora {
   peso = signal(40);
   altura = signal(1.77);
-  categoria = signal("");
 
   imc = computed(() => {
     const peso = this.peso();
     const altura = this.altura();
-    if (peso > 0 && altura > 0) {
-      const valor = peso / (altura * altura);
-      this.actualizarCategoria(valor);
-      return valor;
-    }
-    return 0;
+    return peso > 0 && altura > 0 ? peso / (altura * altura) : 0;
+  });
+
+  categoria = computed(() => {
+    const imc = this.imc();
+    if (imc < 18.5) return "Bajo peso";
+    if (imc < 25) return "Normal";
+    if (imc < 30) return "Sobrepeso";
+    return "Obesidad";
   });
 
   incrementarPeso() {
@@ -33,17 +36,5 @@ export class Calculadora {
 
   resetearPeso() {
     this.peso.set(40);
-  }
-
-  private actualizarCategoria(imc: number) {
-    if (imc < 18.5) {
-      this.categoria.set("Bajo peso");
-    } else if (imc <24.9) {
-      this.categoria.set("Normal");
-    } else if (imc < 29.9) {
-      this.categoria.set("Sobrepeso");
-    } else {
-      this.categoria.set("Obesidad");
-    }
   }
 }
